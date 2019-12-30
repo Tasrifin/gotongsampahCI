012fce31567fe238cc992cf746d6bb3ed3d2dd7e
tyghaykal <andrecsd9@gmail.com>
2019-12-30 07:33:53 +0700

    fix config for localhost

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://projects.ekal.best/gotongsampah/';
+$config['base_url'] = 'http://localhost/gotongsampah/';
 
 /*
 |--------------------------------------------------------------------------
@@ -377,13 +377,27 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'database';
+/* for upload in server you will need this and set something in db
+$config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 3600;
 $config['sess_save_path'] = 'ci_session';
 $config['sess_match_ip'] = true;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
+
+*/
+
+
+/* for localhost you just need it without any changes */
+
+$config['sess_driver'] = 'files';
+$config['sess_cookie_name'] = 'ci_session';
+$config['sess_expiration'] = 7200;
+$config['sess_save_path'] = null;
+$config['sess_match_ip'] = false;
+$config['sess_time_to_update'] = 300;
+$config['sess_regenerate_destroy'] = false;
 
 /*
 |--------------------------------------------------------------------------
